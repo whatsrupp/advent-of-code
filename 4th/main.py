20fab238a839e6c32b415097c2cdf378bc2e528b
Nick Rupp <nickrupp95@gmail.com>
2020-12-16 13:05:22 +0000

    commit day 4

--- 4th/main.py
+++ 4th/main.py
@@ -1,55 +1,70 @@
-class PassportBatchChecker:
+import re
 
-    current_validity_matrix = None
-    current_line = None
+def validate_field(k, v):
+  if k == "byr":
+    return int(v) and len(v) == 4 and 1920 <= int(v) and int(v) <= 2002
+  elif k == "iyr":
+    return int(v) and len(v) == 4 and 2010 <= int(v) and int(v) <= 2020
+  elif k == "eyr":
+    return int(v) and len(v) == 4 and 2020 <= int(v) and int(v) <= 2030
+  elif k == "hgt":
+    number = int(re.search('\d+',v).group())
+    if bool(re.search('^\d+cm$', v)):
+      return 150 <= number and number <= 193
+    elif bool(re.search('^\d+in$', v)):
+      return 59 <= number and number <= 76
+    else:
+      return False
+  elif k == "hcl":
+    return bool(re.search('^#[a-f0-9]{6}$', v))
+  elif k == "ecl":
+    return bool(re.search('^(amb|blu|brn|gry|grn|hzl|oth)$', v))
+  elif k == "pid":
+    return bool(re.search('^[0-9]{9}$', v))
+  elif k == "cid":
+    return True
+  else:
+    return False
 
-    def __init__(self, batch_file="input.txt"):
-        self.batch_file = batch_file
+def is_passport_valid(passport):
+  is_valid = True
+  for key, value in passport.items():
+    is_valid = validate_field(key, value)
+    print(key, value, is_valid)
+    if not is_valid:
+      return False 
+  return is_valid
 
-    def check_batch(self):
-        batch_file = open(self.batch_file, 'r')
-        lines = batch_file.readlines()
+def check_passport_batch(input_file_name = "input.txt"):
+  input_file = open(input_file_name, 'r')
 
-        blank_validity_matrix = {
-          "byr": False,
-          "iyr": False,
-          "eyr": False,
-          "hgt": False,
-          "hcl": False,
-          "ecl": False,
-          "pid": False
-          # "cid": False
-        }
+  current_passport = {}
+  valid_passport_count = 0
 
-        validity_matrix = blank_validity_matrix.copy()
-        valid_passport_count = 0
+  i = 0
+  for line in input_file:
+    i += 1
+    is_last_line = "\n" not in line
+    is_break_line = bool(re.search('^\n$', line))
 
-        for line in lines:
-          line = line.replace("\n", "")
+    if is_break_line:
+      if is_passport_valid(current_passport):
+        valid_passport_count += 1
+      current_passport = {}
+      print('')
+      continue
 
-          if not line:
-            print(validity_matrix)
-            is_valid = True
-            for value in validity_matrix.values():
-               if value == False:
-                 is_valid = False 
-                 break
-            
-            if is_valid:
-              valid_passport_count += 1
+    line = line.replace("\n", "")
+    fields = line.split(" ")
+    for field in fields:
+      key = field[0:3]
+      value = field[4:]
+      current_passport[key] = value
 
-            validity_matrix = blank_validity_matrix.copy()
-            continue
+    if is_last_line:
+      if is_passport_valid(current_passport):
+        valid_passport_count += 1
+  input_file.close()
+  return valid_passport_count
 
-          fields = line.split(" ")
-          for field in fields:
-            attribute = field[0:3]
-            validity_matrix[attribute] = True
-        
-        return valid_passport_count
-
-
-
-
-passport_batch_checker = PassportBatchChecker()
-print(passport_batch_checker.check_batch())
+print(check_passport_batch())
