c500b6344f325ebdb75e72b7139dd2171f58dc24
Nick Rupp <nickrupp95@gmail.com>
2020-12-17 11:21:34 +0000

    write basic implementation with test

--- 6th/main.py
+++ 6th/main.py
@@ -1,4 +1,27 @@
+import re
 
 def main(input_file_name = "input.txt"):
     custom_forms = open(input_file_name, 'r')
-    print(custom_forms)
+
+    sum_of_yes_answers = 0
+
+    set_of_answered_questions = set()
+    for form in custom_forms:
+      is_last_form = "\n" not in form
+      is_end_of_form_group = bool(re.search('^\n$', form))
+
+      if is_end_of_form_group:
+        print(set_of_answered_questions)
+        sum_of_yes_answers += len(set_of_answered_questions)
+        set_of_answered_questions = set()
+        continue
+
+      for char in form.strip():
+        set_of_answered_questions.add(char)
+
+      if is_last_form:
+        print(set_of_answered_questions)
+        sum_of_yes_answers += len(set_of_answered_questions)
+    
+    return sum_of_yes_answers
+  
--- 6th/test.py
+++ 6th/test.py
@@ -1,16 +1,42 @@
 import main
 import inspect
 
+
 def test_main(mocker):
+  complicated_mock = inspect.cleandoc(f'''
+    abc
 
-  mock_file = inspect.cleandoc(f'''
     a
-    bc
-    d
+    b
+    c
 
-    e
+    ab
+    ac
+
+    a
+    a
+    a
+    a
+
+    b
    '''
-)
+  )
+  multi_line_mock = inspect.cleandoc(f'''
+    abc
 
-  mocker.patch("builtins.open", return_value = mock_file)
-  main.main()
+    a
+    b
+    c
+   '''
+  )
+
+  simple_mock = inspect.cleandoc(f'''
+    abc
+   '''
+  )
+  mock_open = mocker.mock_open(read_data=simple_mock)
+  mocker.patch("builtins.open", mock_open)
+  result = main.main()
+  expected = 3
+  assert(result == expected)
+
