d895a20518af48bacdbbdd35f77c2e385a03a3ce
Nick Rupp <nickrupp95@gmail.com>
2020-12-17 11:58:22 +0000

    approach task 2

--- 6th/main.py
+++ 6th/main.py
@@ -1,6 +1,6 @@
 import re
 
-def main(input_file_name = "input.txt"):
+def task1(input_file_name = "input.txt"):
     custom_forms = open(input_file_name, 'r')
 
     sum_of_yes_answers = 0
@@ -11,7 +11,6 @@ def main(input_file_name = "input.txt"):
       is_end_of_form_group = bool(re.search('^\n$', form))
 
       if is_end_of_form_group:
-        print(set_of_answered_questions)
         sum_of_yes_answers += len(set_of_answered_questions)
         set_of_answered_questions = set()
         continue
@@ -20,8 +19,46 @@ def main(input_file_name = "input.txt"):
         set_of_answered_questions.add(char)
 
       if is_last_form:
-        print(set_of_answered_questions)
         sum_of_yes_answers += len(set_of_answered_questions)
     
     return sum_of_yes_answers
   
+
+def task2(input_file_name = "input.txt"):
+    custom_forms = open(input_file_name, 'r')
+
+    sum_of_yes_answers = 0
+
+    answered_questions = {}
+    count_passengers = 0
+
+    def count_answers():
+      count = 0
+      for value in answered_questions.values():
+        if count_passengers == value:
+          count += 1
+      return count
+
+
+    for form in custom_forms:
+      is_last_form = "\n" not in form
+      is_end_of_form_group = bool(re.search('^\n$', form))
+
+      if is_end_of_form_group:
+        sum_of_yes_answers += count_answers()
+        count_passengers = 0
+        answered_questions = {}
+        continue
+
+      count_passengers += 1
+
+      for char in form.strip():
+        if char in answered_questions:
+          answered_questions[char] += 1
+        else:
+          answered_questions[char] = 1
+
+      if is_last_form:
+        sum_of_yes_answers += count_answers()
+    
+    return sum_of_yes_answers
