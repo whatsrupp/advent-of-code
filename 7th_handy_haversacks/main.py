f14e5d9f1596144f4766becfa464d0783fc2f84c
Nick Rupp <nickrupp95@gmail.com>
2020-12-18 14:08:52 +0000

    add recursive search

--- 7th_handy_haversacks/main.py
+++ 7th_handy_haversacks/main.py
@@ -1,5 +1,5 @@
 
-def parse_rules(input_file_name="input.txt"):
+def parse_bags(input_file_name="input.txt"):
   bags = {}
   lines = open(input_file_name, 'r')
 
@@ -18,7 +18,7 @@ def parse_rules(input_file_name="input.txt"):
       amount = tokens[0]
       child_bag_colour = "_".join(tokens[1:3])
       rules.append({
-        "amount": amount,
+        "amount": int(amount),
         "colour": child_bag_colour
       })
       
@@ -29,12 +29,29 @@ def parse_rules(input_file_name="input.txt"):
 
 
 def task1():
-  rules = parse_rules()
+  bags = parse_bags()
 
-  # for bag in rules.keys
+  def can_contain_target(bag_colour):
+    target_bag_colour = "shiny_gold"
+
+    rules = bags[bag_colour]
+
+    if len(rules) == 0:
+      return False
+
+    for rule in rules:
+      if rule["colour"] == target_bag_colour:
+        return True
+      else:
+        return can_contain_target(rule["colour"])
+
+  bag_count = 0
+  for bag_colour in bags.keys():
+    if can_contain_target(bag_colour):
+      bag_count += 1
+
     
-  print(rules)
-  return None
+  return bag_count
 
 
 
